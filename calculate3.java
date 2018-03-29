a917e922a0c8c41cf22e79e7f9ccb13c837f6d51
gsvict1966 <gsvict1966@gmail.com>
2018-03-29 12:45:54 +0300

    create examples

---
+++ calculate3.java
@@ -0,0 +1,13 @@
+public class calculate3 {
+	
+	public static void main( String[] arg ){
+		System.out.println("calculate ...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int third = Integer.valueOf(arg[2]);
+		int sum = first+second+third;
+		System.out.println("summa "+" "+sum);
+	}
+		
+}
+	
